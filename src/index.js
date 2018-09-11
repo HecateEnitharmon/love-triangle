1daeb156781b2a1410ba87fb09edd9ae4d1ab114
enitharmon <hecateenitharmon@gmail.com>
2018-09-11 20:31:08 +0300

    love-triangle/fix: implement love-triangle function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    var count = 0;
+    for (var i = 0; i < preferences.length; i++) {
+        var a = preferences[i];
+        var b = preferences[a - 1];
+        var c = preferences[b - 1];
+        if (c === i + 1) {
+            count++;
+        }
+    }
+    return Math.floor(count/3);
 };
